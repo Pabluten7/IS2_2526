b9b2f1ccc82442f938adb2c55d7111d9eb8d8376
Pabluten7 <pablutenct2@gmail.com>
2026-03-09 17:35:54 +0100

    compile correcto, reviso ejecutable

--- Practica2/Seguros/src/main/java/es/unican/is2/seguros/GestionSeguros.java
+++ Practica2/Seguros/src/main/java/es/unican/is2/seguros/GestionSeguros.java
@@ -1,61 +1,86 @@
 package es.unican.is2.seguros;
 
-// Implementamos las DOS interfaces que nos pide el sistema
 public class GestionSeguros implements IGestionSeguros, IGestionClientes, IInfoSeguros {
-    
-    // Variables para guardar los accesos a datos
+
     private IClientesDAO clientesDAO;
     private ISegurosDAO segurosDAO;
 
-    // Constructor que recibe los dos DAOs (Esto soluciona el primer error)
     public GestionSeguros(IClientesDAO clientesDAO, ISegurosDAO segurosDAO) {
         this.clientesDAO = clientesDAO;
         this.segurosDAO = segurosDAO;
     }
 
     @Override
-    public Cliente nuevoCliente(Cliente c) throws DataAccessException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'nuevoCliente'");
-    }
-
-    @Override
-    public Cliente bajaCliente(String dni) throws OperacionNoValida, DataAccessException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'bajaCliente'");
-    }
-
-    @Override
-    public Seguro nuevoSeguro(Seguro s, String dni) throws OperacionNoValida, DataAccessException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'nuevoSeguro'");
-    }
-
-    @Override
-    public Seguro bajaSeguro(String matricula, String dni) throws OperacionNoValida, DataAccessException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'bajaSeguro'");
-    }
-
-    @Override
-    public Seguro anhadeConductorAdicional(String matricula, String conductor) throws DataAccessException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'anhadeConductorAdicional'");
-    }
-
-    @Override
     public Cliente cliente(String dni) throws DataAccessException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'cliente'");
+        return clientesDAO.cliente(dni);
     }
 
     @Override
     public Seguro seguro(String matricula) throws DataAccessException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'seguro'");
+        return segurosDAO.seguroPorMatricula(matricula);
     }
 
-    // --- A PARTIR DE AQUÍ TIENES QUE GENERAR LOS MÉTODOS ---
-    
-    
+    @Override
+    public Cliente nuevoCliente(Cliente c) throws DataAccessException {
+        return clientesDAO.creaCliente(c);
+    }
+
+    @Override
+    public Cliente bajaCliente(String dni) throws OperacionNoValida, DataAccessException {
+        Cliente c = clientesDAO.cliente(dni);
+        if (c == null) {
+            return null;
+        }
+        if (!c.getSeguros().isEmpty()) {
+            throw new OperacionNoValida("No se puede borrar el cliente porque tiene seguros");
+        }
+        return clientesDAO.eliminaCliente(dni);
+    }
+
+    @Override
+    public Seguro nuevoSeguro(Seguro s, String dni) throws OperacionNoValida, DataAccessException {
+        Cliente c = clientesDAO.cliente(dni);
+        if (c == null) {
+            return null;
+        }
+        Seguro existente = segurosDAO.seguroPorMatricula(s.getMatricula());
+        if (existente != null) {
+            throw new OperacionNoValida("El seguro ya existe");
+        }
+        c.getSeguros().add(s);
+        clientesDAO.actualizaCliente(c);
+        return segurosDAO.creaSeguro(s);
+    }
+
+    @Override
+    public Seguro bajaSeguro(String matricula, String dni) throws OperacionNoValida, DataAccessException {
+        Seguro s = segurosDAO.seguroPorMatricula(matricula);
+        Cliente c = clientesDAO.cliente(dni);
+        if (s == null || c == null) {
+            return null;
+        }
+        boolean pertenece = false;
+        for (Seguro seguroCliente : c.getSeguros()) {
+            if (seguroCliente.getMatricula().equals(matricula)) {
+                pertenece = true;
+                break;
+            }
+        }
+        if (!pertenece) {
+            throw new OperacionNoValida("El seguro no pertenece al cliente");
+        }
+        c.getSeguros().remove(s);
+        clientesDAO.actualizaCliente(c);
+        return segurosDAO.eliminaSeguro(s.getId());
+    }
+
+    @Override
+    public Seguro anhadeConductorAdicional(String matricula, String conductor) throws DataAccessException {
+        Seguro s = segurosDAO.seguroPorMatricula(matricula);
+        if (s == null) {
+            return null;
+        }
+        s.setConductorAdicional(conductor);
+        return segurosDAO.actualizaSeguro(s);
+    }
 }
