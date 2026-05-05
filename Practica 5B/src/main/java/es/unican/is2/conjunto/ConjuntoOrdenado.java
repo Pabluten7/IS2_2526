ab020bc4a3f1da977138d681aad46a85a980024f
Pabluten7 <pablutenct2@gmail.com>
2026-05-05 12:33:37 +0200

    build success 1

--- Practica 5B/src/main/java/es/unican/is2/conjunto/ConjuntoOrdenado.java
+++ Practica 5B/src/main/java/es/unican/is2/conjunto/ConjuntoOrdenado.java
@@ -4,41 +4,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 import es.unican.is2.adt.IConjuntoOrdenado;
+
 /**
  * Clase que implementa el ADT ListaOrdenada
  */
 public class ConjuntoOrdenado<E extends Comparable<E>> implements IConjuntoOrdenado<E> {
 
-	private List<E> lista = new ArrayList<E>();
+    private List<E> lista = new ArrayList<E>();
 
-	public E get(int indice) {
-		return lista.get(indice);
-	}
+    public E get(int indice) {
+        return lista.get(indice);
+    }
 
-	public boolean add(E elemento) {
-		int indice = 0;
-		if (elemento==null)
-			throw new NullPointerException();
-		if (lista.size() != 0) {
-			while (indice < lista.size() && elemento.compareTo(lista.get(indice)) < 0) {
-				indice++;
-			}
-		}
-		lista.add(indice, elemento);
-		return true;
-	}
+    public boolean add(E elemento) {
+        int indice = 0;
+        if (elemento == null)
+            throw new NullPointerException();
+            
+        // CORRECCIÓN 1
+        if (lista.contains(elemento)) {
+            return false;
+        }
 
-	public E remove(int indice) {
-		E borrado = lista.remove(indice);
-		return borrado;
-	}
+        if (lista.size() != 0) {
+            // CORRECCIÓN 2
+            while (indice < lista.size() && elemento.compareTo(lista.get(indice)) > 0) {
+                indice++;
+            }
+        }
+        lista.add(indice, elemento);
+        return true;
+    }
 
-	public int size() {
-		return lista.size();
-	}
+    public E remove(int indice) {
+        E borrado = lista.remove(indice);
+        return borrado;
+    }
 
-	public void clear() {
-		lista.clear();
-	}
+    public int size() {
+        return lista.size();
+    }
+
+    public void clear() {
+        lista.clear();
+    }
 
 }
