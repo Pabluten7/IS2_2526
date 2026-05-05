e55d5d73b53e2dcdfab03c226f35b297bd3ca8cb
Pabluten7 <pablutenct2@gmail.com>
2026-05-05 14:36:13 +0200

    terminado sin clean

--- Practica 5B/src/test/java/es/unican/is2/conjunto/ConjuntoOrdenadoTest.java
+++ Practica 5B/src/test/java/es/unican/is2/conjunto/ConjuntoOrdenadoTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
 public class ConjuntoOrdenadoTest {
     
     private ConjuntoOrdenado<Integer> conjunto;
